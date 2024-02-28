c5472db264a5ace4bc9a65270f7cd27f56cd6bf5
Spotnag <spotnag@hotmail.com>
2024-02-28 16:07:41 +0000

    fixed issue where compiler was removing mul's due to not using the result

--- performance.go
+++ performance.go
@@ -1,6 +1,7 @@
 package main
 
 // #include <simd.c>
+//#cgo LDFLAGS: -Wl,--allow-multiple-definition
 import "C"
 import (
 	"fmt"
@@ -29,16 +30,16 @@ func init() {
 }
 
 func main() {
-	simdIntrinsics()
-	simdGensimd()
 	unrolled()
 	unrolled_nobouncchecking()
+	simdGensimd()
+	simdIntrinsics()
 }
 
 func simdIntrinsics() {
 	start := time.Now()
 	C.add_arrays((*C.float)(unsafe.Pointer(&a[0])), (*C.float)(unsafe.Pointer(&b[0])), C.int(len(a)))
-	fmt.Printf("SIMD Intrinsics - Bil ops/second: %v\n", float64(arrLen)/time.Since(start).Seconds()/1000000000)
+	fmt.Printf("SIMD Intrinsics - Bil ops/second: %v\nSum:%v\n", float64(arrLen)/time.Since(start).Seconds()/1000000000, a[0]+a[1]+a[2]+a[3])
 }
 
 func simdGensimd() {
@@ -48,19 +49,20 @@ func simdGensimd() {
 		a := simd.MulF32x4(simd.F32x4{a[i], a[i+1], a[i+2], a[i+3]}, simd.F32x4{b[i], b[i+1], b[i+2], b[i+3]})
 		sum += a[0] + a[1] + a[2] + a[3]
 	}
-	fmt.Printf("SIMD gensimd - Bil ops/second: %v\n", float64(arrLen)/time.Since(start).Seconds()/1000000000)
+	fmt.Printf("SIMD gensimd - Bil ops/second: %v\nSum:%v\n", float64(arrLen)/time.Since(start).Seconds()/1000000000, sum)
 }
 
 func unrolled() {
 	start := time.Now()
 	sum := float32(0)
 	for i := 0; i < len(a); i += 4 {
-		sum += a[i] * b[i]
-		sum += a[i+1] * b[i+1]
-		sum += a[i+2] * b[i+2]
-		sum += a[i+3] * b[i+3]
+		s0 := a[i] * b[i]
+		s1 := a[i+1] * b[i+1]
+		s2 := a[i+2] * b[i+2]
+		s3 := a[i+3] * b[i+3]
+		sum += s0 + s1 + s2 + s3
 	}
-	fmt.Printf("Unrolled - Bil ops/second: %v\n", float64(arrLen)/time.Since(start).Seconds()/1000000000)
+	fmt.Printf("Unrolled - Bil ops/second: %v\nSum:%v\n", float64(arrLen)/time.Since(start).Seconds()/1000000000, sum)
 }
 
 func unrolled_nobouncchecking() {
@@ -75,5 +77,5 @@ func unrolled_nobouncchecking() {
 		s3 := aTmp[3] * bTmp[3]
 		sum += s0 + s1 + s2 + s3
 	}
-	fmt.Printf("Unrolled no bound checking - Bil ops/second: %v\n", float64(arrLen)/time.Since(start).Seconds()/1000000000)
+	fmt.Printf("Unrolled no bound checking - Bil ops/second: %v\nSum:%v\n", float64(arrLen)/time.Since(start).Seconds()/1000000000, sum)
 }
--- simd.c
+++ simd.c
@@ -1,11 +1,13 @@
 #include <xmmintrin.h>
 
 void add_arrays(float* a, float* b, int len) {
-    __m128 va, vb, vsum;
+    __m128 va, vb, vsum, total = _mm_setzero_ps();
     for (int i = 0; i < len; i += 4) {
         va = _mm_load_ps(a + i);
         vb = _mm_load_ps(b + i);
         vsum = _mm_mul_ps(va, vb);
-        _mm_store_ps(a + i, vsum);
+        total = _mm_add_ps(total, vsum);
+//        _mm_store_ps(a + i, vsum);
     }
+    _mm_store_ps(a, total);
 }
